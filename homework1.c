4d01ebc74876f76590ae8ae6ffbfa1a5b016557b
Jaesub Kim <exima07s@gmail.com>
2024-04-13 23:28:31 +0200

    modified:   bulid/homework1.exe 	modified:   homework1.c

--- homework1.c
+++ homework1.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Exercise 2.2.4 Multiple choice
+
 int mc()
 {
     int random_number;
     int a,res,ans,ipt;
     srand(time(NULL));
     random_number = rand() % 3 + 1; // generate random integer between 1-3
-    printf("%d\n",random_number);
     printf("give an integer: ");
     scanf("%d", &a);
 
@@ -29,18 +30,42 @@ int mc()
     }
 
     printf("1. %d\n2. %d\n3. %d\n", a*3,a*10,a*a);
+
+    // task 2
+/*
+    do{
+        printf("give your answer: \n");
+        scanf("%d",&ipt);
+
+        if (ipt == ans){
+            printf("that's right!");
+            return 0; // return and terminate the program if the answer is correct
+        } else{
+            printf("that's wrong.\n");
+            printf("the right answer is %d.",ans);
+            printf("try again.\n");
+        }
+    } while (1);  // iterate, until correct answer is given
+*/
     printf("give your answer: \n");
     scanf("%d",&ipt);
+
     if (ipt == ans){
-        printf("that's right!");
+        printf("that's right!\n");
+        return 1; // return and terminate the program if the answer is correct
     } else{
         printf("that's wrong.\n");
-        printf("the right answer is %d.",ans);
+        printf("the right answer is %d.\n",ans);
+        return 0;
     }
-
 }
 
 int main(void)
 {
-    mc();
+    int count;
+    for (int i = 1; i <=3; i++){
+        count += mc();
+    }
+    printf("Total number of correct answers: %d\n", count);
+    return 0;
 }
