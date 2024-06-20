7525a45eef932c9d743258158cf0bcd9635f041a
Jaesub Kim <exima07s@gmail.com>
2024-06-20 11:31:42 +0200

    modified:   .vscode/settings.json 	new file:   bulid/poplex.exe 	new file:   exercisecpp4.cpp 	new file:   include/ghc/filesystem.hpp 	new file:   include/ghc/fs_fwd.hpp 	new file:   include/ghc/fs_impl.hpp 	new file:   include/ghc/fs_std.hpp 	new file:   include/ghc/fs_std_fwd.hpp 	new file:   include/ghc/fs_std_impl.hpp 	new file:   include/popl.hpp 	modified:   kim14_pexkan-2.cpp 	new file:   pex.cpp 	new file:   pex.hpp 	new file:   poplex.cpp

--- kim14_pexkan-2.cpp
+++ kim14_pexkan-2.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <cstdlib>
 
 class PexBan{
 private:
@@ -120,6 +121,7 @@ public:
 			std::cout << "invalid list '" << list << "'\n";
 		}
 	}
+	
 
 	void menu () {	
 		std::string choice = "";
@@ -166,8 +168,26 @@ public:
 	}
 };
 
+namespace pex {
+	
+    std::string get_home_directory() {
+        // get home directory Unix
+        const char* homeDir = getenv("HOME");
+        // Get home directory on Windows (untested)
+        const char* userProfile = getenv("USERPROFILE");
+        std::string result = "";
+        if (homeDir != nullptr) {
+            result = homeDir;
+        } else if (userProfile != nullptr) {
+            result = userProfile;
+        }
+        return(result);
+    }
+};
+
 
 int main(int argc, char **argv) {
+	std::cout << "PEX Home: " << pex::get_home_directory() << std::endl;
 	auto kanban = PexBan();
 	kanban.itemAdd("t","first item");
 	kanban.menu();
